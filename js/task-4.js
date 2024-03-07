b85040f2af43460098a9efe012063d86e82d914d
Serhiiowl <sergtsimbal@gmail.com>
2024-03-07 23:22:02 +0200

    hw7

--- js/task-4.js
+++ js/task-4.js
@@ -1,47 +1,22 @@
-const getTotalBalanceByGender = (users, gender) =>
-    users.filter(user => user.gender === gender).reduce((total, client) =>
-        total + client.balance, 0);
+document.addEventListener('DOMContentLoaded', () => {
+  const loginForm = document.querySelector('.login-form');
 
-        
-// Перевірка
-const clients = [
-	{
-    name: "Moore Hensley",
-    gender: "male",
-    balance: 2811
-  },
-  {
-    name: "Sharlene Bush",
-    gender: "female",
-    balance: 3821
-  },
-  {
-    name: "Ross Vazquez",
-    gender: "male",
-    balance: 3793
-  },
-  {
-    name: "Elma Head",
-    gender: "female",
-    balance: 2278
-  },
-  {
-    name: "Carey Barr",
-    gender: "male",
-    balance: 3951
-  },
-  {
-    name: "Blackburn Dotson",
-    gender: "male",
-    balance: 1498
-  },
-  {
-    name: "Sheree Anthony",
-    gender: "female",
-    balance: 2764
-  }
-];
+  loginForm.addEventListener('submit', event => {
+    event.preventDefault(); // Запобігає перезавантаженню сторінки
 
-console.log(getTotalBalanceByGender(clients, "male")); // 12053
+    const formData = new FormData(event.currentTarget);
+    const user = {};
 
-console.log(getTotalBalanceByGender(clients, "female")); // 8863
+    for (const [name, value] of formData) {
+      if (!value.trim()) {
+        alert('All form fields must be filled in');
+        return;
+      }
+      user[name] = value.trim();
+    }
+
+    console.log(user);
+    loginForm.reset(); // Очищає форму після відправлення
+  });
+});
+
